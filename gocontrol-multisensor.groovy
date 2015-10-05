a38887469955ffb0fe0b118f8e5ef90e07cf256a
uncleskippy <sfritzinger@Scotts-MacBook-Pro.local>
2015-10-04 19:04:46 -0700

    Initial commit

---
+++ gocontrol-multisensor.groovy
@@ -0,0 +1,145 @@
+/**
+ *  Copyright 2015 UncleSkippy
+ *
+ *  Based heavily on the Aeon Multisensor template by SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "GoControl Multisensor", namespace: "uncleskippy", author: "UncleSkippy") {
+		capability "Motion Sensor"
+		capability "Temperature Measurement"
+		capability "Configuration"
+		capability "Sensor"
+
+		fingerprint deviceId: "0x2001", inClusters: "0x30,0x31,0x80,0x84,0x70,0x85,0x72,0x86"
+	}
+
+	simulator {
+		// messages the device returns in response to commands it receives
+		status "motion (basic)"     : "command: 2001, payload: FF"
+		status "no motion (basic)"  : "command: 2001, payload: 00"
+		status "motion (binary)"    : "command: 3003, payload: FF"
+		status "no motion (binary)" : "command: 3003, payload: 00"
+
+		for (int i = 0; i <= 100; i += 20) {
+			status "temperature ${i}F": new physicalgraph.zwave.Zwave().sensorMultilevelV2.sensorMultilevelReport(
+				scaledSensorValue: i, precision: 1, sensorType: 1, scale: 1).incomingMessage()
+		}
+	}
+    
+    preferences {
+	    section("Configuration") {
+            input "numMinutes", "number", title: "Motion retrigger delay", defaultValue:2, required: false
+        }
+   	}
+
+	tiles(scale: 2) {
+		multiAttributeTile(name:"motion", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
+				attributeState "active", label:"motion", icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
+				attributeState "inactive", label:"no motion", icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+			}
+		}
+		valueTile("temperature", "device.temperature", inactiveLabel: false, width: 2, height: 2) {
+			state "temperature", label:'${currentValue}°',
+			backgroundColors:[
+				[value: 31, color: "#153591"],
+				[value: 44, color: "#1e9cbb"],
+				[value: 59, color: "#90d2a7"],
+				[value: 74, color: "#44b621"],
+				[value: 84, color: "#f1d801"],
+				[value: 95, color: "#d04e00"],
+				[value: 96, color: "#bc2323"]
+			]
+		}
+		standardTile("configure", "device.configure", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
+		}
+
+		main(["motion", "temperature"]);
+		details(["motion", "temperature", "configure"]);
+	}
+}
+
+// Parse incoming device messages to generate events
+def parse(String description)
+{
+	def result = []
+	//log.debug "GoControl Multisensor parse"
+	def cmd = zwave.parse(description, [0x31: 2, 0x30: 1, 0x84: 1])
+	if (cmd) {
+		if( cmd.CMD == "8407" ) { result << new physicalgraph.device.HubAction(zwave.wakeUpV1.wakeUpNoMoreInformation().format()) }
+		result << createEvent(zwaveEvent(cmd))
+	}
+	//log.debug "Parse returned ${result}"
+	return result
+}
+
+// Event Generation
+def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd)
+{
+	[descriptionText: "${device.displayName} woke up", isStateChange: false]
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.sensormultilevelv2.SensorMultilevelReport cmd)
+{
+	def map = [:]
+	//log.debug "GoControl Multisensor sensor multilevel report"
+	switch (cmd.sensorType) {
+		case 1:
+			// temperature
+			def cmdScale = cmd.scale == 1 ? "F" : "C"
+			map.value = convertTemperatureIfNeeded(cmd.scaledSensorValue, cmdScale, cmd.precision)
+			map.unit = getTemperatureScale()
+			map.name = "temperature"
+			break;
+	}
+	map
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.sensorbinaryv1.SensorBinaryReport cmd) {
+	def map = [:]
+	//log.debug "GoControl Multisensor sensor binary report"
+	map.value = cmd.sensorValue ? "active" : "inactive"
+	map.name = "motion"
+	if (map.value == "active") {
+		map.descriptionText = "$device.displayName detected motion"
+	} else {
+		map.descriptionText = "$device.displayName motion has stopped"
+	}
+	map
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
+	def map = [:]
+	//log.debug "GoControl Multisensor sensor report"
+	map.value = cmd.value ? "active" : "inactive"
+	map.name = "motion"
+	if (map.value == "active") {
+		map.descriptionText = "$device.displayName detected motion"
+	} else {
+		map.descriptionText = "$device.displayName motion has stopped"
+	}
+	map
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	[:]
+	//log.debug "Catchall reached for cmd: ${cmd.toString()}}"
+}
+
+def configure() {
+	//log.debug "GoControl Multisensor configure: numMinutes=${numMinutes}"
+	delayBetween([
+		zwave.configurationV1.configurationSet(parameterNumber: 1, size: 2, scaledConfigurationValue: numMinutes).format(),
+	])
+}
